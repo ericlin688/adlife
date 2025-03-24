9b2440823f4355436bb0094e11719eda4e99304d
ericlin688 <82857659+ericlin688@users.noreply.github.com>
2025-03-24 08:02:07 -0700

    class work done week of 3/17/2025

---
+++ pythonspring25/3_17.py
@@ -0,0 +1,27 @@
+#March 17, 2025
+
+#first function
+#def talk():
+#    print("I come in peace")
+#    return
+
+#talk()
+###############
+
+#def squareit(number):
+#    result = number**2
+#    return result
+
+#x = squareit(4)
+
+#print(x)
+
+# importing
+
+#import random
+#import random as r
+from random import randint
+
+bob = randint(0,1000000)
+
+print(bob)
